7e1dbeadb539663eefe4181373b25593d00bb48d
Subhankar Patra <subhankarpatra258@gmail.com>
2025-09-01 02:37:23 +0530

    Add debugging to index.js and specify Node version for Render

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // Simple proxy to server/index.js for Render deployment
-require('./server/index.js');
+console.log('Starting server proxy...');
+console.log('Node version:', process.version);
+console.log('Working directory:', process.cwd());
+
+try {
+  console.log('Loading server from ./server/index.js');
+  require("./server/index.js");
+  console.log('Server loaded successfully');
+} catch (error) {
+  console.error('Error loading server:', error);
+  process.exit(1);
+}
